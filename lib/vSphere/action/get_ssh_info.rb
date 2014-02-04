099ebafe248ef2b932bf2d8ce1764ffa0ae1ee0a
Manuel Correa <Manuel.Correa@ypg.com>
2014-02-04 10:53:08 -0500

    experimental change - ssh info returning template IP. Changing method to return instead the defined network config in Vagrantfile

--- lib/vSphere/action/get_ssh_info.rb
+++ lib/vSphere/action/get_ssh_info.rb
@@ -20,8 +20,24 @@ module VagrantPlugins
 
         private
 
+        def get_defined_private_network(machine)
+          private_networks = machine.config.vm.networks.find_all { |n| n[0].eql? :private_network }
+          if private_networks.length > 0 then
+            #return the first IP listed in config
+            return private_networks[0][1][:ip]
+          end
+          return nil
+        end
+
         def get_ssh_info(connection, machine)
           return nil if machine.id.nil?
+
+          defined = get_defined_private_network(machine)
+
+          return {
+              :host => defined,
+              :port => 22
+          } if defined
 
           vm = get_vm_by_uuid connection, machine
 
